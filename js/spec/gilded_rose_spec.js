24af937d1ead9ec45cfd5655f137fe6101b37ab4
Rich Williams <rich@toneworm.com>
2019-10-24 20:23:55 +0100

    Created GildedRose class with static methods. Created unit tests for updateQuality method

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,78 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", () => {
+  const { createItem, updateQuality } = GildedRose
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
+  describe("updateQuality", () => {
+    describe("Standard item", () => {
+      const items = [
+        createItem("Bacon sandwich", 15, 8),
+        createItem("Chicken sandwich", 0, 0),
+        createItem("Turkey sandwich", -2, 10),
+      ]
 
-});
+      updateQuality(items)
+
+      // Bacon sandwich
+      it("should degrade the quality", () => {
+        expect(items[0].quality).toEqual(7)
+      })
+
+      it("should reduce the sell_in day", () => {
+        expect(items[0].sell_in).toEqual(14)
+      })
+
+      // Chicken sandwich
+      it("should not degrade the quality if it is already 0", () => {
+        expect(items[1].quality).toEqual(0)
+      })
+
+      // Turkey sandwich
+      it("should degrade the quality twice as fast if sell_in date is negative", () => {
+        expect(items[2].quality).toEqual(8)
+      })
+    })
+
+    describe("Aged Brie", () => {
+      const items = [
+        createItem("Aged Brie", 15, 30),
+        createItem("Aged Brie", 10, 50)
+      ]
+      
+      updateQuality(items)
+
+      it("should increase quality by 1", () => {
+        expect(items[0].quality).toEqual(31)
+      })
+
+      it("should not increase quality above 50", () => {
+        expect(items[1].quality).toEqual(50)
+      })
+    })
+
+    describe("Backstage passes to a TAFKAL80ETC concert", () => {
+      const items = [
+        createItem("Backstage passes to a TAFKAL80ETC concert", 15, 25),
+        createItem("Backstage passes to a TAFKAL80ETC concert", 10, 25),
+        createItem("Backstage passes to a TAFKAL80ETC concert", 5, 25),
+        createItem("Backstage passes to a TAFKAL80ETC concert", 0, 25)
+      ]
+
+      updateQuality(items)
+
+      it("should increase quality by 1 when sell_in greater than 10 days", () => {
+        expect(items[0].quality).toEqual(26)
+      })
+
+      it("should increase quality by 2 when sell_in greater than 5 days and less than or equal to 10 days", () => {
+        expect(items[1].quality).toEqual(27)
+      })
+
+      it("should increase quality by 3 when sell_in less than or equal to 5 days", () => {
+        expect(items[2].quality).toEqual(28)
+      })
+
+      it("should reduce quality to 0 when sell_in is equal to 0 days", () => {
+        expect(items[3].quality).toEqual(0)
+      })
+    })
+  })
+})
